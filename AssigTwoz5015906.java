d584296bfdfe5cbc78c79b396a5292ac7466364e
Matthew Van Schellebeeck <mvanschellebeeck@gmail.com>
2019-11-09 19:51:18 +1100

    Code cleanup

--- AssigTwoz5015906.java
+++ AssigTwoz5015906.java
@@ -10,51 +10,65 @@ import java.util.stream.Collectors;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
+
 public class AssigTwoz5015906 {
 
     private final static Logger logger1 = Logger.getLogger("org");
     private final static Logger logger2 = Logger.getLogger("akka");
-    private final static String BAR_SEPARATOR = "|";
-    private final static String BAR_SEAPRATOR_SPLIT = "\\" + BAR_SEPARATOR;
+    private final static String FIELD_SEPARATOR = " ";
     private final static String NODE_WEIGHT_SEPARATOR = ":";
     private final static String PATH_START = "$";
+    private final static String EMPTY_LIST = "[]";
 
-    // enum for index of lists?
+    public static class Neighbour implements Serializable {
+        String nodeId;
+        Integer distanceFromParent;
+
+        Neighbour(String _nodeId, Integer _distanceFromParent) {
+            nodeId = _nodeId;
+            distanceFromParent = _distanceFromParent;
+        }
+
+        String getNodeId() {
+            return nodeId;
+        }
+
+        Integer getDistanceFromParent() {
+            return distanceFromParent;
+        }
+    }
 
     public static class Node implements Serializable, Comparable<Node> {
         String nodeId;
         Integer distanceFromSource;
-        ArrayList<String> adjacencyList;
+        ArrayList<Neighbour> adjacencyList;
         String bestPathToNode;
 
-        public ArrayList<String> parseAdjacencyList(String list) {
-            ArrayList<String> result = new ArrayList<>();
-            if (list.equals("[]")) return result;
+        ArrayList<Neighbour> parseAdjacencyList(String list) {
+            ArrayList<Neighbour> result = new ArrayList<>();
+            if (list.equals(EMPTY_LIST)) return result;
             String[] cleanList = list
-                    .replace("[", "")
-                    .replace("]", "")
-                    .split(",");
+                .replace("[", "")
+                .replace("]", "")
+                .split(",");
 
-           for (int i = 0; i < cleanList.length; i++) {
-                String[] pairs = cleanList[i].split(NODE_WEIGHT_SEPARATOR);
-                result.add(pairs[0] + ":" + pairs[1]);
+            for (String s : cleanList) {
+                String[] pairs = s.split(NODE_WEIGHT_SEPARATOR);
+                result.add(new Neighbour(pairs[0], Integer.parseInt(pairs[1])));
             }
-           return result;
+            return result;
         }
 
-        Node(String nodeId, Integer distanceFromSource,
-             ArrayList<Tuple2<String, Integer>> adjacencyList, String path) {
-            this.nodeId = nodeId;
-            this.distanceFromSource = distanceFromSource;
-            this.adjacencyList = (ArrayList<String>) adjacencyList
-                    .stream()
-                    .map(t -> String.join(":", t._1, t._2.toString()))
-                    .collect(Collectors.toList());
-            this.bestPathToNode = path;
+        Node(String _nodeId, Integer _distanceFromSource,
+             ArrayList<Neighbour> _adjacencyList, String _path) {
+            nodeId = _nodeId;
+            distanceFromSource = _distanceFromSource;
+            adjacencyList = _adjacencyList;
+            bestPathToNode = _path;
         }
 
         Node(String line) {
-            String[] tokens = line.split(BAR_SEAPRATOR_SPLIT);
+            String[] tokens = line.split(" ");
             nodeId = tokens[0];
             distanceFromSource = Integer.parseInt(tokens[1]);
             adjacencyList = parseAdjacencyList(tokens[2]);
@@ -69,24 +83,25 @@ public class AssigTwoz5015906 {
             return distanceFromSource;
         }
 
-        ArrayList<Tuple2<String, Integer>> getAdjacencyList() {
-            return (ArrayList<Tuple2<String, Integer>>) adjacencyList.stream().map(s -> {
-               String[] tokens = s.split(":") ;
-               return new Tuple2<>(tokens[0], Integer.parseInt(tokens[1]));
-            }).collect(Collectors.toList());
+        ArrayList<Neighbour> getAdjacencyList() {
+            return adjacencyList;
         }
 
-        public String getBestPathToNode() {
+        String getBestPathToNode() {
             return bestPathToNode;
         }
 
         @Override
         public String toString() {
-            return String.join("|",
-                    nodeId,
-                    distanceFromSource.toString(),
-                    adjacencyList.toString().replaceAll("\\s", ""),
-                    bestPathToNode);
+            ArrayList<String> parseList = (ArrayList<String>) adjacencyList.stream()
+                .map(n -> String.join(":", n.getNodeId(), n.getDistanceFromParent().toString()))
+                .collect(Collectors.toList());
+
+            return String.join(" ",
+                nodeId,
+                distanceFromSource.toString(),
+                parseList.toString().replaceAll("\\s", ""),
+                bestPathToNode);
         }
 
         @Override
@@ -102,36 +117,41 @@ public class AssigTwoz5015906 {
     }
 
     private static JavaRDD<String> formatInput(JavaRDD<String> input, String startNode) {
-        // change to
-        // Node   DistanceToSource Neighbours           Path
-        // NO     0                [(N1,4),(N2,3)]      ""
-        // N1     -1                [(N1,4),(N2,3)]      ""
-        // ...
-        return input.mapToPair(
-                line -> {
-                    String [] parts = line.split(",");
-                    String firstNode = parts[0];
-                    String lastNode = parts[1];
-                    Integer distance = Integer.parseInt(parts[2]);
-                    return new Tuple2<>(
-                            firstNode,
-                            lastNode + ":" + distance
-                    );
-                }
-        ).groupByKey().map(pair ->
-            String.join(BAR_SEPARATOR,
-                    pair._1,
-                    pair._1.equals(startNode) ? "0" : "-1",
-                    pair._2.toString().replaceAll("\\s", ""),
-                    PATH_START
+        return input.mapToPair(line -> {
+            String[] tokens = line.split(",");
+            String firstNode = tokens[0];
+            String lastNode = tokens[1];
+            int distance = Integer.parseInt(tokens[2]);
+            return new Tuple2<>(firstNode, lastNode + ":" + distance);
+        }).groupByKey().map(pair ->
+            String.join(FIELD_SEPARATOR,
+                pair._1,
+                pair._1.equals(startNode) ? "0" : "-1",
+                pair._2.toString().replaceAll("\\s", ""),
+                PATH_START
             )
         );
     }
 
-
+    private static ArrayList<Tuple2<String, Node>> processNeighbours(Node node) {
+        ArrayList<Tuple2<String, Node>> result = new ArrayList<>();
+        result.add(new Tuple2<>(node.getNodeId(), node));
+        Integer distance = node.getDistanceFromSource();
+        if (distance > -1) {
+            for (Neighbour neighbour : node.getAdjacencyList()) {
+                String bestPath = node.getBestPathToNode();
+                String newPath = bestPath.equals(PATH_START) ? "" : bestPath + "-";
+                // can't view neighbours of neighbours
+                ArrayList<Neighbour> neighbours = new ArrayList<>();
+                result.add(new Tuple2<>(neighbour.getNodeId(),
+                            new Node(neighbour.getNodeId(), distance + neighbour.getDistanceFromParent(),
+                            neighbours, newPath + node.getNodeId())));
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-
 
         String startNode = args[0];
         String inputPath = args[1];
@@ -141,92 +161,59 @@ public class AssigTwoz5015906 {
         logger2.setLevel(Level.OFF);
 
         JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("Ass2").setMaster("local"));
+        int iterationNo = 0;
+
+        // parse input to desired format
         JavaRDD<String> input = sc.textFile(inputPath);
-
-        JavaRDD<String> parsed = formatInput(input, startNode);
-
-        JavaRDD<Node> graph = parsed.mapToPair(line -> {
-            Node node = new Node(line);
-            return new Tuple2<>(node.getNodeId(), node);
-        }).values();
-
-        graph.collect().forEach(System.out::println);
-        graph.saveAsTextFile("iteration0");
+        formatInput(input, startNode).saveAsTextFile("iteration" + iterationNo);
 
         HashMap<Integer, Boolean> visited = new HashMap<>();
-        int index = 0;
 
         while (visited.size() != 6) {
-            JavaRDD<String> prevGraph = sc.textFile("iteration" + index);
-            index += 1;
-            System.out.println("Previous iteration:");
-            prevGraph.collect().forEach(System.out::println);
+            JavaRDD<String> graph = sc.textFile("iteration" + iterationNo);
+            iterationNo += 1;
 
             // mapper (read text and convert back to key pair)
             JavaPairRDD<String, Node> mapper =
-                    prevGraph
-                    .flatMapToPair(line-> {
-                        ArrayList<Tuple2<String, Node>> result = new ArrayList<>();
-                        Node node = new Node(line);
-                        result.add(new Tuple2<>(node.getNodeId(), node));
-                        if (node.getDistanceFromSource() >= 0) {
-                            Integer distance = node.getDistanceFromSource();
-                            for (Tuple2<String, Integer> neighbour : node.getAdjacencyList()) {
-                                result.add(new Tuple2<>(neighbour._1,
-                                        new Node(neighbour._1, distance + neighbour._2, new ArrayList<>(),
-                                                (node.getBestPathToNode().equals(PATH_START) ? "" : node.getBestPathToNode() + "-") + node.getNodeId())));
-                            }
-                        }
-                        return result.iterator();
-                    });
+                graph.flatMapToPair(line -> {
+                    Node node = new Node(line);
+                    ArrayList<Tuple2<String, Node>> valuesToBeEmitted = processNeighbours(node);
+                    return valuesToBeEmitted.iterator();
+                });
 
-            mapper.reduceByKey((node, node2) ->{
+            // reducer (group by keys and take best path/min dist)
+            mapper.reduceByKey((node, node2) -> {
                 // take adjacency list from the original file (one line of every iteration)
-                ArrayList<Tuple2<String, Integer>> neighbours = node.getAdjacencyList().isEmpty() ?
+                ArrayList<Neighbour> neighbours = node.getAdjacencyList().isEmpty() ?
                         node2.getAdjacencyList() : node.getAdjacencyList();
 
-                Integer minDistance;
-                String path;
+                // select node with min distance from source
+                return node.compareTo(node2) > 0 ?
+                        new Node(node.getNodeId(), node.getDistanceFromSource(), neighbours,
+                            node.getBestPathToNode())
+                        : new Node(node.getNodeId(), node2.getDistanceFromSource(), neighbours,
+                            node2.getBestPathToNode());
 
-               if (node.compareTo(node2) == 1) {
-                  minDistance = node.getDistanceFromSource();
-                  path = node.getBestPathToNode();
-                } else {
-                   minDistance = node2.getDistanceFromSource();
-                   path = node2.getBestPathToNode();
-               }
+            }).values().saveAsTextFile("iteration" + iterationNo);
 
-                return new Node(node.getNodeId(), minDistance, neighbours, path);
-            }).values().saveAsTextFile("iteration" + (index));
-
-            visited.put(index, true);
+            visited.put(iterationNo, true);
         }
 
-                sc.textFile("iteration" + index).filter(line -> {
-            String[] tokens = line.split(BAR_SEAPRATOR_SPLIT);
+        // take last iteration, sort by distance, complete path (by appending final node)
+        sc.textFile("iteration" + iterationNo).filter(line -> {
+            String[] tokens = line.split(FIELD_SEPARATOR);
             return !tokens[0].equals(startNode);
-        }).map(line ->
-            {
-                String[] tokens = line.split(BAR_SEAPRATOR_SPLIT);
-                String finishNode = tokens[0];
-                String distance = tokens[1];
-                String path = tokens[3];
-
-                return String.join(",", finishNode, distance,  path);
-            }
-        ).mapToPair(line -> {
-            String[] tokens = line.split(",");
-            Integer distance = Integer.parseInt(tokens[1]);
-            Tuple2<String, String> nodeAndPath = new Tuple2<>(tokens[0], tokens[2]);
-            return new Tuple2<>(distance, nodeAndPath);
+        }).mapToPair(line -> {
+            Node node = new Node(line);
+            Tuple2<String, String> nodeAndPath = new Tuple2<>(node.getNodeId(), node.getBestPathToNode());
+            return new Tuple2<>(node.getDistanceFromSource(), nodeAndPath);
         }).sortByKey().map(pair -> {
             String node = pair._2._1;
             String path = pair._2._2;
-            Integer distance = pair._1;
-            return path.equals(PATH_START) ? String.join(",", node, distance.toString())
-                    : String.join(",", node, distance.toString(), path + "-" + node);
+            String distance = pair._1.toString();
+            return path.equals(PATH_START) ?
+                    String.join(",", node, distance)
+                    : String.join(",", node, distance, path + "-" + node);
         }).saveAsTextFile(outputPath);
-
-
     }
 }
--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 INPUT_FILE="graph2.txt"
 OUTPUT_FILE="the_output"
-START_NODE="B"
+START_NODE="A"
 rm -rf iteration* $OUTPUT_FILE
 javac -cp ".:Spark-Core.jar" AssigTwoz5015906.java 
 java -cp ".:Spark-Core.jar" AssigTwoz5015906 $START_NODE $INPUT_FILE $OUTPUT_FILE 
